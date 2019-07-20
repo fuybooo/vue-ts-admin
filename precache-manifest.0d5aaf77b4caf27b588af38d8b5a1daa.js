ac585b0fb75dbe11aa098b5caef1efaa2873d907
fuyongbo <fuyongbo@everjiankang.com>
2019-07-20 17:38:41 +0800

    chore: 构建到github

---
+++ precache-manifest.0d5aaf77b4caf27b588af38d8b5a1daa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "5506aca9807a7c0ac3072dd2de77b171",
+    "url": "/vue-ts-admin/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-ts-admin/robots.txt"
+  },
+  {
+    "revision": "43058c68c2d9f0792042caf7e98210ce",
+    "url": "/vue-ts-admin/mock/demo/table/create.json"
+  },
+  {
+    "revision": "61d4c043a6f1801b4c0a",
+    "url": "/vue-ts-admin/js/chunk-vendors.c9475c3f.js"
+  },
+  {
+    "revision": "d54df857e13fd59a490927d33a55175b",
+    "url": "/vue-ts-admin/mock/demo/table/getById.json"
+  },
+  {
+    "revision": "4ad45766d88bdaa210a004e88d59284d",
+    "url": "/vue-ts-admin/mock/platform/login.json"
+  },
+  {
+    "revision": "9a4540367fc9b85746eb4e20e9fce88e",
+    "url": "/vue-ts-admin/mock/demo/table/get.json"
+  },
+  {
+    "revision": "6682b53aae4458179bb909d71ab7d842",
+    "url": "/vue-ts-admin/mock/demo/table/edit.json"
+  },
+  {
+    "revision": "0075f7b9d5ccb54572ae0396b163bfe4",
+    "url": "/vue-ts-admin/mock/demo/table/del.json"
+  },
+  {
+    "revision": "9023cb98277e1007fff6",
+    "url": "/vue-ts-admin/js/lang-en.cef41184.js"
+  },
+  {
+    "revision": "d21b3dd4da0427695257",
+    "url": "/vue-ts-admin/js/app.daf81e63.js"
+  },
+  {
+    "revision": "a39b74ba3f6017ddf79282fdcf644326",
+    "url": "/vue-ts-admin/img/login_bg.a39b74ba.jpg"
+  },
+  {
+    "revision": "13c44e0970a51f9ad4c8ddc9f76534f7",
+    "url": "/vue-ts-admin/img/login.13c44e09.png"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-ts-admin/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-ts-admin/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "d21b3dd4da0427695257",
+    "url": "/vue-ts-admin/css/app.644bb326.css"
+  }
+];
